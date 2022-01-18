c5a02f9f72864a10c566a824e1beb964d7940c55
HalilHopa <halilhp@gmail.com>
2022-01-18 21:35:30 +0100

    flask-01 and flask-02 added

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -0,0 +1,21 @@
+from re import T
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1 = 7000, number2 = 9000)
+
+@app.route('/mult')
+def number():
+    var1, var2 = 300, 700
+    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
